06f47c00089bd05180f06945f281a8c31c65c645
Thanh Loi <thanhtranloi1912@gmail.com>
2026-06-12 10:38:48 +0700

    play sound

---
+++ src/lesson12.cpp
@@ -0,0 +1,41 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
+int main(){
+    sf::ContextSettings settings;
+    settings.antiAliasingLevel = 4;
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML", sf::State::Windowed, settings);
+    //window.setKeyRepeatEnabled(false);
+    sf::SoundBuffer buffer_jump("Sounds/Jump.mp3");
+    sf::Sound sound_jump(buffer_jump);
+    sound_jump.setVolume(50.f);
+    sound_jump.setLooping(true);
+    sound_jump.setPitch(0.5f);
+    
+    while(window.isOpen()){
+        while(auto event = window.pollEvent()){
+            if(event->is<sf::Event::Closed>()){
+                window.close();
+            }
+            
+            else if(event->is<sf::Event::Resized>()){
+                sf::View view(sf::FloatRect({0.f, 0.f}, sf::Vector2f(window.getSize())));
+                window.setView(view);
+            }
+
+            else if(auto key = event->getIf<sf::Event::KeyPressed>()){
+                if(key->scancode == sf::Keyboard::Scancode::Space){
+                    sound_jump.play();
+                }
+            }
+        }
+     
+        window.clear({64, 64, 64});
+      
+        window.display();
+    }
+    return 0;
+}
